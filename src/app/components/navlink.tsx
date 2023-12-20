323218444214d287813b0361a5b003b521f9361b
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2023-12-19 23:19:48 -0500

    Fix Navlink padding

--- src/app/components/navlink.tsx
+++ src/app/components/navlink.tsx
@@ -9,7 +9,7 @@ type navlinksprops = {
 const Navlink: React.FC<navlinksprops> = ({ href, title, function1}) => {
 	return (
 		<Link
-			className="text-[#ff8949] pl-3 pr-4 sm:text-xl rounded md:p-0 font-bold
+			className="text-[#ff8949] pl-2 pr-4 sm:text-xl rounded md:p-0 font-bold
 							transition-all duration-500 transform md:hover:px-6 md:hover:py-2 md:hover:m-4
 							hover:px-3 hover:py-1 hover:m-2
 							hover:shadow-lg hover:bg-[#ff8949] hover:text-white"
