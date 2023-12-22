473baea40fa3b30a3013a5e871bf1efe8adb7092
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2023-12-22 11:12:44 -0500

    Add xs media query

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,43 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-	darkMode: 'class',
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      fontFamily: {
-				poppins: ['Poppins', 'sans-serif'],
-				urbanist: ['Urbanist', 'sans-serif'],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	darkMode: "class",
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
-    },
-  },
-  plugins: [],
-}
-export default config
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+				urbanist: ["Urbanist", "sans-serif"],
+			},
+			screens: {
+				xs: "375px",
+				sm: "640px",
+				// => @media (min-width: 640px) { ... }
+
+				md: "768px",
+				// => @media (min-width: 768px) { ... }
+
+				lg: "1024px",
+				// => @media (min-width: 1024px) { ... }
+
+				xl: "1280px",
+				// => @media (min-width: 1280px) { ... }
+
+				"2xl": "1536px",
+				// => @media (min-width: 1536px) { ... }
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
