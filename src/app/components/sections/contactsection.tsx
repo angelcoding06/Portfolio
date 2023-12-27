9764b956692b5425a5543c8d52f47b40629f71ea
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2023-12-26 23:22:39 -0500

    Create contactsection

---
+++ src/app/components/sections/contactsection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ContactSection = () => {
+	return (
+		<section
+			className="flex flex-col justify-between scroll-my-28 py-12 border-solid border-t-2 border-orange-600 "
+			id="contact"
+		>
+			<h1 className="font-extrabold text-center text-mainOrange mb-8 text-4xl lg:text-5xl lg:leading-normal justify-center">
+				Contact
+			</h1>
+		</section>
+	);
+};
+
+export default ContactSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,16 @@ import Navbar from "./components/navbar";
 import Aboutsection from "./components/sections/aboutsection";
 import HomeSection from "./components/sections/homesection";
 import ProjectSection from "./components/sections/projectsection";
+import ContactSection from "./components/sections/contactsection";
 export default function Home() {
 	return (
-		<main className="flex min-h-screen bg-mainWhite dark:bg-pageBlack">
+		<main className="flex min-h-screen bg-mainWhite dark:bg-pageBlack transition-all duration-300">
 			<Navbar />
 			<div className="container mt-24 mx-auto px-5 py-4 sm:px-12 lg:px-28">
 				<HomeSection />
 				<Aboutsection />
 				<ProjectSection />
-				<div id="contact" className="h-screen bg-amber-500">
-					<h1>Section 3</h1>
-				</div>
+				<ContactSection />
 			</div>
 		</main>
 	);
