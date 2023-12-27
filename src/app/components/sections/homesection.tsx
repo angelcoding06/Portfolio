b104f661c62869a640ccb9a1c30af55d5431b875
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2023-12-26 22:55:44 -0500

    Add subtitle

--- src/app/components/sections/homesection.tsx
+++ src/app/components/sections/homesection.tsx
@@ -11,12 +11,15 @@ const HomeSection = () => {
 		<section className="pt-4 pb-24 sm:py-12 lg:py-48 scroll-my-48" id="home">
 			<div className="flex flex-col md:flex-row items-center justify-between">
 				<div className="md:pr-12 pb-5 md:pb-0">
-					<h1 className="font-extrabold text-black dark:text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal justify-center">
+					<h1 className="font-extrabold text-black dark:text-white mb-2 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal justify-center">
 						<span>Hello!</span>
 						<span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-mainOrange">
 							{Title}
 						</span>
 					</h1>
+					<h2 className=" font-semibold text-black dark:text-white mb-4 text-xl sm:text-2xl lg:text-3xl lg:leading-normal justify-center">
+						Building software for humans, one line of code at a time.
+					</h2>
 					<p className="font-poppins text-black dark:text-white text-base sm:text-lg mb-6 lg:text-xl">
 						{text}
 					</p>
