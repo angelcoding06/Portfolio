b8c3ca992a0a93bb8613dd7ab599c6bbd868db57
angelcoding06 <vasquezgerenaangelmateo@gmail.com>
2023-12-27 23:15:18 -0500

    Change favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
 	title: "My Portfolio",
 	description: "angelcoding06's portfolio",
+	icons: {
+		icon: "/favicon.ico",
+	},
 };
 
 export default function RootLayout({
